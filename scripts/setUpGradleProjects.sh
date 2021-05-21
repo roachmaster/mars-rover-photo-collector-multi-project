cf728e034942f554ccd9b303d7694a0c36892d25
Leonardo Rocha <leonardo.rocha1990@gmail.com>
2021-05-21 16:18:39 -0500

    adding initial scripts dir

---
+++ scripts/setUpGradleProjects.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function isGradleProject(){
+    [[ -d "$1" && "$1" != "commonGradleFiles" && "$1" != "scripts" ]]
+}
+
+function listGradleProjects(){
+    for f in * ;
+        do isGradleProject $f && 
+	   echo $f ; 
+    done
+}
+
+function copyGradleFilesToProject() {
+    for f in * ;
+        do isGradleProject $f &&
+           cp -r commonGradleFiles/* $f ;
+    done
+}
+
+echo "Moving files from commonGradleFiles to the following directories"
+listGradleProjects
